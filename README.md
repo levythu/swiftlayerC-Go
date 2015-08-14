c051c9ca1cbf2864ed245b5a1d89df340f5cf2cf
Levy <zly.george@163.com>
2015-08-14 16:12:21 +0800

    start to implement outapi.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # swiftlayerC-Go
 The Golang version of Swift Layer-C
+
+**Remember to set environment variable SLCHOME to locate the homepath.**
---
+++ src/outapi/iobase.go
@@ -0,0 +1,10 @@
+// Define the output interface to contect with Openstack Swift.
+// Also, it can be rewrite to connect to local disk or other storage media.
+// Pay attention that the input/output data is assembled, for streaming version
+// Please refer to [TODO:Set it up!]
+
+package outapi
+
+type Outapi interface {
+
+}
