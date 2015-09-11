24f7723feccb6ab03bb3cc65c73cca26452eb2b0
Levy <zly.george@163.com>
2015-09-11 15:44:25 +0800

    swiftio infrastructure

--- src/outapi/iobase.go
+++ src/outapi/iobase.go
@@ -1,7 +1,7 @@
 // Define the output interface to contect with Openstack Swift.
 // Also, it can be rewrite to connect to local disk or other storage media.
 // Pay attention that the input/output data is assembled, for streaming version
-// Please refer to [TODO:Set it up!]
+// Please refer to put/getStream
 
 package outapi
 
@@ -12,7 +12,7 @@ import (
 
 type Outapi interface {
     generateUniqueID() string
-    
+
     put(filename string, content filetype.Filetype, info FileMeta) error
 
     // If file does not exist, a nil will be returned. No error occurs.
@@ -24,4 +24,6 @@ type Outapi interface {
     getinfo(filename string) (FileMeta, error)
 
     delete(filename string) error
+
+    //TODO: Setup streaming api.
 }
--- src/outapi/swift.go
+++ src/outapi/swift.go
@@ -2,4 +2,52 @@ package outapi
 
 import (
     "github.com/ncw/swift"
+    "definition/configinfo"
 )
+
+struct SwiftConnector {
+    c *swift.Connection
+}
+// If auth failed, return nil
+func ConnectbyAuth(username string, passwd string, tenant string) *SwiftConnector {
+    swc:=&swift.Connection{
+        UserName: username,
+        ApiKey: passwd,
+        Tenant: tenant,
+        AuthUrl: configinfo.GetProperty_Node("swift_proxy_url").(string),
+    }
+    if swc.Authenticate()!=nil {
+        return nil
+    }
+
+    return &SwiftConnector{
+        c: swc
+    }
+}
+func ConnectbyPreauth(account string, token string) *SwiftConnector {
+    // TODO: set it up!
+    return nil
+}
+
+struct Swiftio {
+    conn *SwiftConnector
+    container string
+}
+
+// 2 ways to setup a new swift io.
+func NewSwiftio(_conn *SwiftConnector, _container string) *Swiftio {
+    return &Swiftio{
+        conn: _conn,
+        container: _container,
+    }
+}
+func DupSwiftio(oldio *Swiftio, _container string) *Swiftio {
+    return &Swiftio{
+        conn: oldio.conn,
+        container: _container,
+    }
+}
+
+func (this *Swiftio)generateUniqueID string {
+    return "outapi.Swiftio: "+this.container
+}
