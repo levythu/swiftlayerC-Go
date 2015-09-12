306dfd46ec29aae5059808c08f123a5ca4cf5b41
Levy <zly.george@163.com>
2015-09-12 21:22:06 +0800

    add support for comment(/**/) in JSON file. Add comment in configure file/.

--- src/definition/configinfo/jsoninit.go
+++ src/definition/configinfo/jsoninit.go
@@ -4,7 +4,35 @@ import (
     "io/ioutil"
     . "definition"
     "encoding/json"
+    "bytes"
+    "fmt"
 )
+
+// Removes the /**/ line from the string
+func RemoveSlashCommentLine(str []byte) []byte {
+    var i=0
+    var j, k int
+    lim:=len(str)
+    res:=[]byte{}
+    sep:=[]byte("/*")
+    ed:=[]byte("*/")
+    for i<lim {
+        j=bytes.Index(str[i:], sep)
+        if j==-1 {
+            res=append(res, str[i:]...)
+            break
+        }
+        res=append(res, str[i:i+j]...)
+        k=bytes.Index(str[i+j:], ed)
+        //fmt.Println(i,",",j,",",k)
+        if k==-1 {
+            break
+        }
+        i=i+j+k+len(ed)
+    }
+
+    return res
+}
 
 //Pay attention that filename is a relative path
 func ReadFileToJSON(filename string) (map[string]Tout, error) {
@@ -18,6 +46,10 @@ func ReadFileToJSON(filename string) (map[string]Tout, error) {
     if err!=nil {
         return nil, err
     }
+
+    fmt.Println(string(res))
+    res=RemoveSlashCommentLine(res)
+    fmt.Println(string(res))
 
     var ret map[string]Tout
     err=json.Unmarshal(res, &ret)
