d31884d6f6efbc366a8a0582739973a580b570f4
Levy <zly.george@163.com>
2015-10-11 14:48:37 +0800

    worked out test

--- src/kernel/filesystem/fs_test.go
+++ src/kernel/filesystem/fs_test.go
@@ -2,22 +2,35 @@ package filesystem
 
 import (
     "testing"
+    "time"
+    "fmt"
 )
 
 func TestSession(t *testing.T) {
     var ses4test=NewSession(Testio)
     res, _:=ses4test.Ls()
-    t.Log(res);
+    fmt.Println(res);
+
+    fmt.Println(ses4test.Mkdir("花花花"))
 
     ses4test.Cd("..")
     res, _=ses4test.Ls()
-    t.Log(res);
+    fmt.Println(res);
 
     ses4test.Cd("huahua")
     res, _=ses4test.Ls()
-    t.Log(res);
+    fmt.Println(res);
+
+    ses4test.Cd("实验2")
+    res, _=ses4test.Ls()
+    fmt.Println(res);
+    fmt.Println(ses4test.Mkdir("asd asdjld asd"))
 
     ses4test.Cd("..")
     res, _=ses4test.Ls()
-    t.Log(res);
+    fmt.Println(res);
+
+    for {
+        time.Sleep(time.Hour)
+    }
 }
