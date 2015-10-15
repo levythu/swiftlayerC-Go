41dd41449c751571355d247cfa03714404b4de50
Levy <zly.george@163.com>
2015-10-15 10:05:43 +0800

    add dependency

--- bin/introdependencies.bat
+++ bin/introdependencies.bat
@@ -3,3 +3,6 @@
 
 @go get github.com/ncw/swift
 @ECHO Successfully get package github.com/ncw/swift
+
+@go get github.com/levythu/gurgling
+@ECHO Successfully get package github.com/levythu/gurgling
--- bin/introdependencies.sh
+++ bin/introdependencies.sh
@@ -7,3 +7,6 @@ echo "Successfully change path to $GOPATH"
 
 go get github.com/ncw/swift
 echo "Successfully get package github.com/ncw/swift"
+
+go get github.com/levythu/gurgling
+echo "Successfully get package github.com/levythu/gurgling"
