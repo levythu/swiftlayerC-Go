9993461b8386b0d3dd2e19ce23935befa1cd10c1
Levy <zly.george@163.com>
2016-03-01 16:18:52 +0800

    update #4

--- src/kernel/distributedvc/filehandler.go
+++ src/kernel/distributedvc/filehandler.go
@@ -5,6 +5,9 @@ import (
     _ "logger"
     "kernel/filetype"
     . "outapi"
+    "strconv"
+    . "definition/configinfo"
+    . "utils/timestamp"
 )
 
 type FD struct {
@@ -28,7 +31,13 @@ type FD struct {
     /*====BEGIN: for functionality====*/
     updateChainLock *sync.RWMutex
     nextAvailablePosition int
+
+    // only available when active
+    numberZero *filetype.Kvmap
+    contentLock *sync.RWMutex
 }
+
+// Lock priority: lock > updateChainLock > contentLock
 
 const (
     INTRA_PATCH_METAKEY_NEXT_PATCH="next-patch"
@@ -47,6 +56,9 @@ func newFD(filename string, io Outapi) *FD {
 
         updateChainLock: &sync.RWMutex{},
         nextAvailablePosition: -1,
+
+        numberZero: nil,
+        contentLock: &sync.RWMutex{},
     }
     ret.trashNode=&fdDLinkedListNode {
         carrier: ret,
@@ -99,15 +111,79 @@ func (this *FD)GoGrasped() {
     this.LoadPointerMap()
 }
 
+func (this *FD)ReadInNumberZero() error {
+
+}
+
+/*
+** Patch list: 0(the combined version) -> 1 -> 2 -> ...
+** If the #0 patch does not exist, the file does not have a separate version in the node.
+** otherwise, "INTRA_PATCH_METAKEY_NEXT_PATCH" in the meta will form a linked list
+** to chain all the uncombined patch.
+**
+** As soon as the file is loaded into system, its uncombined patch will start to combine
+** and the dormant fd will store the next available patch number.
+*/
+
+func (this *Fd)GetPatchName(patchnumber int, nodenumber int/*-1*/) string {
+    if nodenumber<0 {
+        nodenumber=NODE_NUMBER
+    }
+    return this.filename+".node"+strconv.Itoa(nodenumber)+".patch"+strconv.Itoa(patchnumber)
+}
+
 func (this *FD)LoadPointerMap() error {
+    this.lock.Lock()
+    defer this.lock.Unlock()
+
     this.updateChainLock.Lock()
     defer this.updateChainLock.Unlock()
 
     if this.nextAvailablePosition>=0 {
         return nil
     }
+
+    var tmpPos=0
+    for {
+        tMeta, tErr:=this.io.Getinfo(this.GetPatchName(tmpPos, -1))
+        if tErr!=nil {
+            return tErr
+        }
+        if tMeta==nil {
+            // the file does not exist
+            this.nextAvailablePosition=tmpPos
+            return nil
+        }
+        if tNum, ok:=tMeta[INTRA_PATCH_METAKEY_NEXT_PATCH]; !ok {
+            Secretary.WarnD("File "+this.filename+"'s patch #"+tmpPos+" has broken/invalid metadata. All the patches after it will get lost.")
+            this.nextAvailablePosition=tmpPos+1
+            return nil
+        } else {
+            tmpPos, tErr=strconv.Atoi(tNum)
+            // TODO: consider add it into merge list
+        }
+    }
+
     return nil
 }
 func (this *FD)Submit(object *filetype.Kvmap) error {
-    return nil
+    this.updateChainLock.RLock()
+    defer this.updateChainLock.RUnlock()
+
+    if this.nextAvailablePosition<0 {
+        panic("Fatal logic error!")
+    }
+    var err=this.io.Put(this.GetPatchName(this.nextAvailablePosition, -1),
+                object,
+                FileMeta(map[string]string {
+                    INTRA_PATCH_METAKEY_NEXT_PATCH: strconv.Itoa(this.nextAvailablePosition+1),
+                    METAKEY_TIMESTAMP: GetTimestamp().String()
+                })
+    )
+    if err!=nil {
+        Secretary.Warn("Fail in putting file "+this.GetPatchName(this.nextAvailablePosition, -1), "FD.Submit()")
+        return err
+    }
+    this.nextAvailablePosition++
+    // TODO: consider add it into merge list
 }
