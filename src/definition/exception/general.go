6071b4331ac6bc0d4eb710a88f1a1dcf45fc12d7
Levy <zly.george@163.com>
2016-03-17 16:12:18 +0800

    fix CEs

--- src/definition/exception/general.go
+++ src/definition/exception/general.go
@@ -3,5 +3,5 @@ package exception
 import "errors"
 
 var (
-    LOGICAL_ERROR: errors.New("Logical error happens and may be very severe: please commit the debugger logs to the author of the code.")
+    LOGICAL_ERROR=errors.New("Logical error happens and may be very severe: please commit the debugger logs to the author of the code.")
 )
