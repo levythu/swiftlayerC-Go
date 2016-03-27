a4742f2dad7b98dd3bec4763b0c257bf5f400238
Levy <zly.george@163.com>
2016-03-27 12:44:09 +0800

    add log for terminating

--- src/inapi/webserver.go
+++ src/inapi/webserver.go
@@ -2,17 +2,24 @@ package inapi
 
 import (
     . "github.com/levythu/gurgling"
+    //"github.com/levythu/gurgling/midwares/analyzer"
     "inapi/containermanage"
     "inapi/fsmanage"
     "inapi/streamio"
     conf "definition/configinfo"
+    . "logger"
 )
 
 func Entry() {
     var rootRouter=ARouter()
+    //rootRouter.Use(analyzer.ASimpleAnalyzer())
+
     rootRouter.Use("/fs", fsmanage.FMRouter())
     rootRouter.Use("/io", streamio.IORouter())
     rootRouter.Use("/cn", containermanage.CMRouter())
 
-    rootRouter.Launch(conf.OUTER_SERVICE_LISTENER)
+    var err=rootRouter.Launch(conf.OUTER_SERVICE_LISTENER)
+    if err!=nil {
+        Secretary.Error("mainpkg::Entry()", "HTTP Server terminated: "+err.Error())
+    }
 }
