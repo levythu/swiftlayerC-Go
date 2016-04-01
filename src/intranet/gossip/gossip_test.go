2f32f28251fd86a7281400ab04510f6d779b0a21
Levy <zly.george@163.com>
2016-04-01 10:19:24 +0800

    update test dependency

--- src/intranet/gossip/gossip_test.go
+++ src/intranet/gossip/gossip_test.go
@@ -4,9 +4,25 @@ import (
     "testing"
     "time"
     "fmt"
+    . "definition"
+    conf "definition/configinfo"
 )
 
 func TestBatRand(t *testing.T) {
+    GlobalGossiper=(func() Gossiper {
+        var ret=NewBufferedGossiper(conf.GOSSIP_BUFFER_SIZE)
+        ret.PeriodInMillisecond=conf.GOSSIP_PERIOD_IN_MS
+        ret.EnsureTellCount=conf.GOSSIP_RETELL_TIMES
+        ret.TellMaxCount=conf.GOSSIP_MAX_DELIVERED_IN_ONE_TICK
+        ret.ParallelTell=conf.GOSSIP_MAX_TELLING_IN_ONE_TICK
+        ret.SetGossiperList([]Tout{1,2,3,4,5})
+        ret.SetGossipingFunc(func(addr Tout, content []Tout) error {
+            fmt.Println(addr, ": ", content)
+            return nil
+        })
+
+        return ret
+    })()
     go GlobalGossiper.Launch()
     var i=0
     for {
